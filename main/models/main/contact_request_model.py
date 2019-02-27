bfd02ddea57d047770f38034d1898fe4e782174b
Jonathan--V <Jonathan--V@users.noreply.github.com>
2019-02-27 16:08:02 +0000

    Fix missing table issue in db
    Solution was `python manage.py migrate --run-syncdb`

--- main/models/main/contact_request_model.py
+++ main/models/main/contact_request_model.py
@@ -5,4 +5,4 @@ class ContactRequestModel(models.Model):
     email_address = models.EmailField()
     name = models.CharField(max_length=100, blank=True)
     subject = models.CharField(max_length=200)
-    description = models.TextField(max_length=250)
+    description = models.TextField(max_length=20000)
